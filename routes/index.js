0bf10523c8060893da4fbde1a759712792f6faf4
Jakub <kubabartak@yahoo.com>
2017-04-11 00:45:51 +0200

    prepared for deployment

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var router = express.Router();
+var ip = require('ip');
+var os = require('os');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function(req, res) {
+    var software = os.type() + " "+ os.release().split('.')[0]+"."+os.release().split('.')[1];
+    var address= ip.address();
+  var languages= req.headers["accept-language"];
+    var language=languages.split(',')[0];
+    
+    data={ IP: address,
+        language: language,
+        software:  software 
+         };
+
+  res.json(data);
 });
 
 module.exports = router;
