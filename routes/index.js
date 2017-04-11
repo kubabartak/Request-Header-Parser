07430ef682655545eea5063c80fffec87156df64
Jakub <kubabartak@yahoo.com>
2017-04-11 02:13:48 +0200

    remote IP

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,11 @@ var os = require('os');
 /* GET home page. */
 router.get('/', function(req, res) {
     var software = os.type() + " "+ os.release().split('.')[0]+"."+os.release().split('.')[1];
-    var address= ip.address();
+    var address= req.headers['x-forwarded-for'] ||
+    req.connection.remoteAddress ||
+    req.socket.remoteAddress ||
+    req.connection.socket.remoteAddress;
+    
   var languages= req.headers["accept-language"];
     var language=languages.split(',')[0];
     
